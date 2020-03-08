eb8caa53a98ae7480f9f0d51c5b967661ad7caf7
annisall <anniina.sallinen@gmail.com>
2020-03-08 11:04:05 +0200

    Added directories into gitignore, added a function for getting a bearer token for making the calls

--- lambda_girls_meetup/project.clj
+++ lambda_girls_meetup/project.clj
@@ -1,4 +1,6 @@
 (defproject data-pipe-line-demo "0.1.0-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [clj-http "3.10.0"]
+                 [proto-repl "0.3.1"]]
   :source-paths ["src"]
   :repl-options {:port 8081})
--- lambda_girls_meetup/src/core.clj
+++ lambda_girls_meetup/src/core.clj
@@ -1,1 +1,7 @@
-(ns core)
+(ns core
+  (:require [clj-http.client :as http]))
+
+(defn get-bearer-token [secrets url]
+  (let [response (http/post url {:basic-auth [(:apikey secrets) (:apisecret secrets)]
+                                 :form-params {:grant_type "client_credentials"}})]
+    (:body response)))
